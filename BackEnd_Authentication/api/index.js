bd1bcae3c682e5f7304f361e7fba711169e23883
pnkkumar123 <pankajkanwar420@gmail.com>
2024-01-31 15:03:56 +0530

    established connection with mongoDB

--- BackEnd_Authentication/api/index.js
+++ BackEnd_Authentication/api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log("connected to mongoDB");
+})
+.catch((error)=>{
+    console.log(error);
+})
 
 const app = express();
 
