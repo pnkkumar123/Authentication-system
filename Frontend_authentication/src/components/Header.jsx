773df52821fa2212e7b2776155442da7e3eaf5da
pnkkumar123 <pankajkanwar420@gmail.com>
2024-01-31 14:02:54 +0530

    create header components and tailwind installed

--- Frontend_authentication/src/components/Header.jsx
+++ Frontend_authentication/src/components/Header.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='bg-gray-300'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+           <Link to='/'>
+           <h1 className='font-bold'>
+                Auth App
+              </h1>
+           </Link>
+            <ul className='flex gap-4'>
+            <Link to='/'><li>Home</li></Link>
+            <Link to='/about'><li>About</li></Link>
+            <Link to='/signin'>
+            <li>Sign_IN</li>
+            </Link>
+            </ul>
+
+        </div>
+        Header
+        </div>
   )
 }
 
