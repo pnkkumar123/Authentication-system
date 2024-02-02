eb512cdffd0cbc3e3178fc8458e7be7fe8def5ff
pnkkumar123 <pankajkanwar420@gmail.com>
2024-02-02 16:15:13 +0530

    complete signup page functionallity

--- Frontend_authentication/src/pages/SignUp.jsx
+++ Frontend_authentication/src/pages/SignUp.jsx
@@ -8,7 +8,8 @@ export default function SignUp() {
     email: '',
     password: ''
   });
-  
+  const [error,setError] = useState(false);
+  const [loading,setLoading] = useState(false);
   const handleChange = (e)=>{
     setFormData({...formData,[e.target.id]:e.target.value})
 
@@ -16,26 +17,27 @@ export default function SignUp() {
   // where async is used use new react updates
   const handleSubmit = async (e)=>{
     e.preventDefault();
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    
-    if (!res.ok) {
-      // Handle non-successful responses
-      console.error('Error:', res.status, res.statusText);
+    try{
+      setLoading(true);
+      setError(false);
+  const res = await fetch('/api/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  }); 
+    const data = await res.json();
+    setLoading(false);
+    if(data.sucess === false){
+      setError(true);
       return;
     }
-    
-    try {
-      const data = await res.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-    }
+ }catch(error){
+  setLoading(false);
+  setError(true);
+ }
+   
     
     
   }
@@ -47,13 +49,14 @@ export default function SignUp() {
       <input type="text" onChange={handleChange} name="username" id="username" placeholder='UserName' className='bg-slate-300 p-3 rounded-lg'/>
       <input type="email" onChange={handleChange} name="email" id="email" placeholder='email' className='bg-slate-300 p-3 rounded-lg'/>
       <input type="password" onChange={handleChange} name="password" id="password" placeholder='password' className='bg-slate-300 p-3 rounded-lg'/>
-      <button className='bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>SignUp</button>
+      <button disabled={loading} className='bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'loading...' : 'Sign up'}</button>
     
     </form>
     <div className='flex gap-2 mt-5'>
       <p>Have an account?</p>
       <Link to='/signin'><span className='text-blue-500'>Sign-In</span></Link>
     </div>
+    <p className='text-red-700 mt-5'>{error && "something went wrong" }</p>
     </div>
     </>
   )
