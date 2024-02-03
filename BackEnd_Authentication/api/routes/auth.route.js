0b8e6c1925c5f60232489defff0ff5b70e6d849b
pnkkumar123 <pankajkanwar420@gmail.com>
2024-02-03 08:50:55 +0530

    added sign in functionality and added jasonwebtoken.remove password from client side

--- BackEnd_Authentication/api/routes/auth.route.js
+++ BackEnd_Authentication/api/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utilis/error.js';
-
+import jwt from 'jsonwebtoken';
 
 const authRouter = express.Router();
 
@@ -16,6 +16,24 @@ authRouter.post("/signup", async (req,res,next)=>{
    }catch(e){
    next(errorHandler(500,"something went wrong"))
    }
+})
+authRouter.post("/signin", async (req,res,next)=>{
+    const {email,password} = req.body;
+    try{
+      const validUser = await User.findOne({email});
+      if(!validUser) return next (errorHandler(404,'User not found'));
+      const validPassword = bcryptjs.compareSync(password,validUser.password);
+      if(!validPassword)  return next(errorHandler(404,'wrong credentials'))
+      const {password:hashedPassword, ...rest} = validUser._doc ; 
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+    const age = new Date(Date.now() + 3600000);
+       res.cookie('acces_token',token,{httpOnly:true,expires: age},
+       ).status(200)
+       .json(rest);
+          
+    }catch(e){
+        next(e)
+    }
 })
 
 export  {authRouter}
